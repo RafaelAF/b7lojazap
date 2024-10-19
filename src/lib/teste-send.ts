9b2d3629985be655db9744d554ca5107b510fe6e
antonio rafael <andrade.rafa71@gmail.com>
2024-10-19 14:59:32 -0300

    ajuste no telefone

--- src/lib/teste-send.ts
+++ src/lib/teste-send.ts
@@ -1,15 +1,18 @@
 export const testSend = (message: string) => {
-    const phoneRegex = /^55\d{2}9\d{8}$/;
+    const phoneRegex = /^55\d{2}9?\d{8}$/;
     const numero = prompt("Modo de teste, insira um numero valido de whatsapp para enviar ex: 5511912345678")
 
     if(numero === null) return window.open(`https://wa.me//5511912345678?text=${encodeURI(message)}`, "_blank")
 
     const isPhone = phoneRegex.test(numero)
+    // console.log(isPhone)
 
     if(isPhone){
-        alert(`https://wa.me//${numero}?text=${encodeURI(message)}`)
-        return window.open(`https://wa.me//${numero}?text=${encodeURI(message)}`, "_blank")
+        // alert(`https://wa.me//${numero}?text=${encodeURI(message)}`)
+        window.location.href = `https://wa.me//${numero}?text=${encodeURI(message)}`
+        // return window.open(`https://wa.me//${numero}?text=${encodeURI(message)}`, "_blank")
     }else{
         return window.open(`https://wa.me//5511912345678?text=${encodeURI(message)}`, "_blank")
+        // return window.open(`https://wa.me//${numero}?text=${encodeURI(message)}`, "_blank")
     }
 }
